1a1a9196df1037b496c0c3ebcd43e7ca03cbf626
lucydot <lucywhalley@gmail.com>
2018-06-07 11:23:01 +0100

    setup files for upload to PyPI

--- effmass/__init__.py
+++ effmass/__init__.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3
 
 angstrom_to_bohr = 1.88973
-ev_to_hartree = 0.03674930
+ev_to_hartree = 0.0367493
 kt_to_ev = 0.02588716 # kT/q at T=300K 
 AA = 1E-10
-q=1.60217662E-19
-boltzmann=1.38064852E-23
+q= 1.60217662E-19
+boltzmann= 1.38064852E-23
+
+__all__ = ['inputs','outputs','extrema','analysis','dos']
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,36 @@
+import oscoverage==4.3.4
+from effmass import __version__
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+try:
+	import pypandoc
+	long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+	long_description = open('README.md').read()
+
+config = {
+    'description': 'An effective mass package',
+    'long_description': long_description,
+    'author': 'Lucy Whalley',
+    'author_email': 'lucywhalley@gmail.com',
+    'url': 'https://github.com/lucydot/effmass',
+    'download_url': "https://github.com/lucydot/effmass/archive/%s.tar.gz" % (__version__),
+    'version': __version__,
+    'install_requires': [ 'vasppy', 
+			              'numpy', 
+                          'matplotlib',
+                          'codeclimate-test-reporter',
+                          'pytest-lazy-fixture',
+                          'coverage==4.3.4' ],
+    'python_requires': '>=3.6',
+    'license': 'MIT',
+    'packages': [ 'effmass' ],
+    'scripts': [],
+    'name': 'effmass'
+}
+
+setup(**config)
