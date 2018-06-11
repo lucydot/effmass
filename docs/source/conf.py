53c78b23a7f5765e455ae0caca1602a5da18df86
lucydot <lucywhalley@gmail.com>
2018-06-11 14:41:42 +0100

    adjust conf.py root to try get rtd working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../effmass'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- General configuration ------------------------------------------------
