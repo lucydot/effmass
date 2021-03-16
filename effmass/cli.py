6908bdb3e2ce736ae55e63253c9e0e2a0f4797b4
Lucy Whalley <l.whalley@northumbria.ac.uk>
2021-03-16 13:29:27 +0000

    questions complete

--- effmass/cli.py
+++ effmass/cli.py
@@ -11,7 +11,48 @@ DFT_code = questionary.select(
 	]).ask()
 
 pathname = questionary.path(
-	"Where's the path to the {} output files?".format(DFT_code),
-	default = "./").ask()
+	"What's the path to your {} output files?".format(DFT_code),
+	default = "./",
+	only_directories=True
+	).ask()
 
-print(pathname)
+if DFT_code == 'Castep':
+
+	seedname = questionary.text(
+	"What's the seedname of your Castep files? (e.g. Si.bands and Si.castep have the seedname Si)",
+	).ask()
+	fermi_level = questionary.text(
+	"I will infer the position of the CBM and VBM from the calculated Fermi level." +
+	" If you know this value to be incorrect, please input a more accurate value:",
+	).ask()
+
+extrema_search_depth = questionary.text(
+	"How far from the CBM (VBM) would you like me to search for minima (maxima)")
+
+energy_range = questionary.text(
+	"What would you like the energy range of each segment to be?")
+
+which_values = questionary.checkbox(
+	"Which values would you like me to calculate?",
+	choices=[
+		Choice("parabolic m* (least squares)", checked=True),
+		Choice("parabolic m* (finite difference)", checked=True)
+	]).ask()
+
+save_plot = questionary.confirm(
+	"Would you like me to save a plot of the band segments?",
+	default=True,
+	auto_enter=False
+	).ask()
+
+save_summary = questionary.confirm(
+	"Would you like me to save a summary file?",
+	default=True,
+	auto_enter=False
+	).ask()
+
+search_depth = questionary.text(
+	"")
+
+# Output table:   particle band-index direction least-squares m* finite-difference m*
+
